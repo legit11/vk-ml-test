52c3660c1fab3384b92e235982514acd8902b09f
legit11 <125328481+legit11@users.noreply.github.com>
2024-07-14 22:20:45 +0300

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from kafka import KafkaConsumer, KafkaProducer
 import json
-from processor import TDocument, Processor
+import logging
 from collections import defaultdict
 from typing import List, Dict
 
+from kafka import KafkaConsumer, KafkaProducer
 
-consumer = KafkaConsumer(               #Kafka
+from processor import TDocument, Processor
+
+logging.basicConfig(level=logging.INFO)             # логи
+logger = logging.getLogger(__name__)
+
+
+consumer = KafkaConsumer(               # Kafka
     'input_topic',
     bootstrap_servers=['localhost:9092'],
     value_deserializer=lambda m: json.loads(m.decode('utf-8'))
@@ -22,18 +28,19 @@ processor = Processor()
 documents_by_url: Dict[str, List[TDocument]] = defaultdict(list)                # Основной цикл обработки
 
 for message in consumer:
-    incoming_documents = [TDocument.from_dict(doc) for doc in message.value]
+    try:
+        incoming_documents = [TDocument(**doc) for doc in message.value]
 
+        for doc in incoming_documents:
+            documents_by_url[doc.url].append(doc)
 
-    for doc in incoming_documents:              # Группировка по URL
-        documents_by_url[doc.url].append(doc)
+        for url, docs in documents_by_url.items():
+            processed_doc = processor.process(docs)
+            producer.send('output_topic', processed_doc.dict())
+            logger.info(f"Processed Document for URL {url}: {processed_doc}")
 
+        documents_by_url.clear()
+    except Exception as e:
+        logger.error(f"Error processing message: {e}", exc_info=True)
 
-    for url, docs in documents_by_url.items():              # Обработка каждой группы документов
-        processed_doc = processor.process(docs)
-        producer.send('output_topic', processed_doc.to_dict())
-        print(f"Processed Document for URL {url}: {processed_doc}")
-
-
-    documents_by_url.clear()            # Очистка после обработки
 
